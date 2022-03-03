32e67f3c73aece57c447e9a25a1f1c5e0b73e9aa
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2022-03-03 14:26:08 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SolarSystem_GRP5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
