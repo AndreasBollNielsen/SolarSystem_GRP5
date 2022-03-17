6d8036267dd98a180292ab499da102865024fc8c
Sebgineer <sebastian-karlsen@hotmail.com>
2022-03-17 12:10:55 +0100

    Quiz fix

--- wwwroot/js/Fullscreen.js
+++ wwwroot/js/Fullscreen.js
@@ -6,10 +6,15 @@
 ////    }, 5000);
 ////}
 
-$(window).on("load", function () {
-        document.body.requestFullscreen();
-});
 
 function Full() {
+    document.getElementsByClassName("Fullscreen")[0].style.display = "None"
     document.body.requestFullscreen();
 }
+
+addEventListener('fullscreenchange', event => {
+    console.log(event.path[2].fullscreen);
+    if (!event.path[2].fullscreen) {
+        document.getElementsByClassName("Fullscreen")[0].style.display = "block"
+    }
+});
